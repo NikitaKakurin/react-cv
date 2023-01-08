152c5458f0370370e205e89d67530eb31db68553
NikitaKakurin <neyasut87@rambler.ru>
2023-01-08 17:18:17 +0300

    fix: delete flip css

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,35 +4,31 @@ import Portfolio from './Portfolio';
 import Education from './Education';
 import { FlippingPages } from 'flipping-pages';
 import 'flipping-pages/dist/style.css';
-import './flip.css';
-import { useAppDispatch, useAppSelector } from 'app/hooks';
+import { useAppSelector } from 'app/hooks';
 import { setPage } from 'app/actionsCreators/pageActionsCreators';
 import { selectPage } from 'app/slices/pageSlice';
 
 export default function Main() {
-  const dispatch = useAppDispatch();
   const page = useAppSelector(selectPage);
 
   return (
-    <>
-      <div className="pages">
-        <FlippingPages
-          direction="right-to-left"
-          disableSwipe={true}
-          onSwipeEnd={setPage}
-          selected={page}
-          animationDuration={700}
-        >
-          <div className="page page1 flex">
-            <About />
-            <Skills />
-          </div>
-          <div className="page page2 flex">
-            <Education />
-            <Portfolio />
-          </div>
-        </FlippingPages>
-      </div>
-    </>
+    <div className="h-full w-full">
+      <FlippingPages
+        direction="right-to-left"
+        disableSwipe={true}
+        onSwipeEnd={setPage}
+        selected={page}
+        animationDuration={700}
+      >
+        <div className="flex h-full w-full bg-[#EBD5B3]">
+          <About />
+          <Skills />
+        </div>
+        <div className="flex h-full w-full bg-[#EBD5B3]">
+          <Education />
+          <Portfolio />
+        </div>
+      </FlippingPages>
+    </div>
   );
 }
