54073ddee6bd17979603fba7b5e1af5505270522
NikitaKakurin <neyasut87@rambler.ru>
2023-01-09 20:14:49 +0300

    feat: add codewars

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -8,10 +8,13 @@ export default function Portfolio() {
     >
       <div className="p-4">
         <h2 className="text-center font-marck text-4xl">Портфолио:</h2>
-        <div className="relative flex w-full items-center justify-center md:h-[70vh]">
+        <div className="relative flex w-full flex-col items-center justify-center md:h-[70vh]">
           <div className="w-full rounded-lg">
             <Slider />
           </div>
+          <a href="https://www.codewars.com/users/NikitaKakurin/">
+            <img src="https://www.codewars.com/users/NikitaKakurin/badges/small" alt="codewars" />
+          </a>
         </div>
       </div>
     </div>
